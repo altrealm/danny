b9f4d6610632199cb29254822479ab5d0703c456
altrealm <kanatashirai@gmail.com>
2023-04-03 14:04:01 +0700

    commit

--- project-one-main/src/Navbar.js
+++ project-one-main/src/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Signin from "./Signin";
+import Signin from "./Register folder/Register";
+import Register from "./Register folder/Register";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
-import Logout from "./Logout";
+import Logout from "./Register folder/Logout";
 import {Link} from "react-router-dom"
-
+import Profile from "./Profile folder/Profile";
+import ProfileContent from "./Profile folder/ProfileContent";
+import Login from "./Register folder/Login";
 
 function Navbar() {
     const [user] = useAuthState(auth)
@@ -22,6 +25,8 @@ function Navbar() {
            button to gas price
         </button>
         </Link>
+        
+        <Login/>
         </div>
     )
 }
